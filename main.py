b55675916ded4031a26d64c2410d2fc7d1294591
rrabinesh <138852428+rrabinesh@users.noreply.github.com>
2024-04-17 17:55:10 +0530

    final

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from fastapi import FastAPI, HTTPException, Query
 from typing import Optional
 import httpx
+from pywa import WhatsApp
+from pywa.types import Message, Button
 
 app = FastAPI()
 
-@app.post("/send_message/{to}/{template_name}/")
-async def send_message(to: str,template_name: str):
-    access_token = "EAAFtnme63bgBO4dI02BeZBfowFGaSZAcm9sgAVmZA0IarGlnSomZAr2ZARjq3iSFZCnl6AemptVf9918XdAWa1pkuQ8t8ZCA51RrlHYcWSTe4VDF1Y9j6ZBUz7RuL2LXrK8yLdD78FMLQ64vx32ZBF7rZA87BO9tvtQ7ljlTAlqgp386BnzjjTSxwsUxr4R9ZBUPSyd"
-    graph_api_url = "https://graph.facebook.com/v18.0/273242249210974/messages"
+wa = WhatsApp(
+    phone_id='273242249210974',
+    token='EAAFtnme63bgBO4dI02BeZBfowFGaSZAcm9sgAVmZA0IarGlnSomZAr2ZARjq3iSFZCnl6AemptVf9918XdAWa1pkuQ8t8ZCA51RrlHYcWSTe4VDF1Y9j6ZBUz7RuL2LXrK8yLdD78FMLQ64vx32ZBF7rZA87BO9tvtQ7ljlTAlqgp386BnzjjTSxwsUxr4R9ZBUPSyd',
+    server=app,
+    verify_token='hvjchsdjc'
 
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
+)
 
-    request_body = {
-        "messaging_product": "whatsapp",
-        "to": to,
-        "type": "template",
-        "template": {
-            "name": template_name,
-            "language": {
-                "code": "en_US"
-            }
-        }
-    }
+@app.post("/send_message/{to}/")
+async def send_message(to: str):  
+    data = {to:{"Pets":["Tommy","Charlie","Cooper"]}}
+    if isinstance(to, str) and len(to) > 0:
+        pets = data.get(to, {}).get("Pets", [])
+        buttons = [Button(pet, callback_data=pet.lower()) for pet in pets]
 
-    async with httpx.AsyncClient() as client:
-        response = await client.post(graph_api_url, headers=headers, json=request_body)
+        wa.send_message(
+            to=to,
+            text="Choose a pet to book the service",
+            buttons=buttons,
+        )
+    return {"message": "Message sent successfully"}
 
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Failed to send message")
+@app.post("/slot/{to}/")
+async def send_message(to: str): 
+    data = {to:{"Pets":["Morning","Afternoon","Evening"]}} 
+    if isinstance(to, str) and len(to) > 0:
+        pets = data.get(to, {}).get("Pets", [])
+        buttons = [Button(pet, callback_data=pet.lower()) for pet in pets]
 
-    return {"message": "Message sent successfully", "result": response.json()}
+        wa.send_message(
+            to=to,
+            text="Choose a slot to book the service",
+            buttons=buttons,
+        )
+    return {"message": "Message sent successfully"}
